e0bac6f680c810708b21d0f6d3155b87334ff9cf
Naman195 <72151532+Naman195@users.noreply.github.com>
2021-10-03 09:17:39 +0530

    Update vowel-consonant.c

--- C-Programs/vowel-consonant.c
+++ C-Programs/vowel-consonant.c
@@ -2,12 +2,13 @@
 int main() {
     char c;
     int lowercase_vowel, uppercase_vowel;
-    printf("Enter an alphabet: ");
+    printf("Enter an alphabet: "); // Take an input from the user as a character
     scanf("%c", &c);
 
-    lowercase_vowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+    lowercase_vowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'); // check that given input by user is in lower case or not 
 
-    uppercase_vowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+    uppercase_vowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'); // check that given input by user is in upper case or not 
+
 
     if (lowercase_vowel || uppercase_vowel)
         printf("%c is a vowel.", c);
